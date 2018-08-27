2e21f214a64973acfe91ca2354f28b7e089455cf
lazy_learner <robinttt333@gmail.com>
2018-08-27 16:09:07 +0530

    Added models Question and Answer and created superuser with username:Canvass and password:canvass123

--- CanvassVirtual/Canvass/question/models.py
+++ CanvassVirtual/Canvass/question/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+User=get_user_model()
+
+class Question(models.Model):
+    user=models.ForeignKey(User,null=False,on_delete="CASCADE")
+    title=models.CharField(max_length=250)
+    description=models.TextField(max_length=350,null=False,blank=False)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['-created_on']
+
+    def __str__(self):
+        return "{} \n Asked by:{}".format(self.title,self.asked_by.user)
+
+class Answer(models.Model):
+    user=models.ForeignKey(User,null=False,on_delete="CASCADE")
+    question=models.ForeignKey(Question,on_delete="CASCADE")
+    description=models.TextField(null=False,blank=False)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['-created_on']
+
+    def __str__(self):
+        return "{} answered {}".format(self.user.username,self.question.title)
