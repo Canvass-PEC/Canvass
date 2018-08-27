6ce009b3b0ad224467a9ebb4466e628d29f5028f
lazy_learner <robinttt333@gmail.com>
2018-08-27 16:39:10 +0530

    Created model Article

--- CanvassVirtual/Canvass/article/models.py
+++ CanvassVirtual/Canvass/article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User=get_user_model()
+
+class Article(models.Model):
+    user=models.ForeignKey(User,on_delete="CASCADE")
+    title=models.CharField(max_length=200)
+    description=models.TextField(null=False)
+    created_on=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['-created_on']
+
+    def __str__(self):
+        return "{} Written by {}".format(self.title,self.user.username)
