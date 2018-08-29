f153b8d095965296cfc866f9be98547100615ade
lazy_learner <robinttt333@gmail.com>
2018-08-29 17:48:35 +0530

    Added templates in main directory

--- CanvassVirtual/Canvass/question/urls.py
+++ CanvassVirtual/Canvass/question/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from .views import home
+app_name="question"
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('question.urls',namespace='question')),
+    path('',home,name='home'),
 ]
